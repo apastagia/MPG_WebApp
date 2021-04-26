54069761a69034c124beb0ac694138df343f6e90
AayushiPastagia <53651953+AayushiPastagia@users.noreply.github.com>
2021-04-26 17:47:54 +0530

    modified views.py

--- mpgWebApp/firstPage/views.py
+++ mpgWebApp/firstPage/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import pandas as pd
+import joblib
+
+
 # Create your views here.
+reloadModel = joblib.load('./models/RF_MPG.pkl')
+
 def index(request):
     context = {'a':'Hello DjangoFramework'}
     return render(request, 'index.html', context)
@@ -10,6 +16,16 @@ def index(request):
 def predictMPG(request):
     print(request)
     if request.method == 'POST':
-        print(request.POST.dict())
-    context = {'a':'Hello User'}
+        temp = {}
+        temp['cyl'] = request.POST.get('cylinderVal')
+        temp['disp'] = request.POST.get('dispVal')
+        temp['hp'] = request.POST.get('hrsPwrVal')
+        temp['wt'] = request.POST.get('weightVal')
+        temp['acc'] = request.POST.get('accVal')
+        temp['yr'] = request.POST.get('modelVal')
+        temp['origin'] = request.POST.get('originVal')
+    
+    testData = pd.DataFrame({'x':temp}).transpose()
+    scoreval = reloadModel.predict(testData)[0]
+    context = {'scoreval':scoreval}
     return render(request, 'index.html', context)

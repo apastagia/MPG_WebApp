19ef3cf6e663fa719816e3e2b9e16d0598350774
AayushiPastagia <53651953+AayushiPastagia@users.noreply.github.com>
2021-04-27 14:24:13 +0530

    settings.py

--- mpgWebApp/mpgWebApp/settings.py
+++ mpgWebApp/mpgWebApp/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mpgWebApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
